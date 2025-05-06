dc54e94b6743767bb402338f3e279515bd747f7a
jvogel <joevogel501@gmail.com>
2025-05-05 17:53:28 -0700

    springboot initializr

---
+++ src/main/java/us/metabolomics/sirius/sirius_client/SiriusClientApplication.java
@@ -0,0 +1,13 @@
+package us.metabolomics.sirius.sirius_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriusClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriusClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/metabolomics/sirius/sirius_client/SiriusClientApplicationTests.java
@@ -0,0 +1,13 @@
+package us.metabolomics.sirius.sirius_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiriusClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
